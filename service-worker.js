e11c11d3fc43d1affcabe2d701fe1484b1d713fc
Always Trying To Be Better <103570217+FTN-Dev@users.noreply.github.com>
2025-10-09 04:40:34 +0700

    service worker

--- service-worker.js
+++ service-worker.js
@@ -10,13 +10,15 @@ self.addEventListener('activate', () => {
 });
 
 self.addEventListener('message', event => {
+  console.log("📨 Message received in Service Worker:", event.data);
   const data = event.data;
   if (data && data.type === 'NEW_TASK') {
     const { title, assigner } = data.payload;
+    console.log("🔔 Showing notification for:", title);
     self.registration.showNotification('🆕 Topik Baru Ditambahkan!', {
       body: `${title} oleh ${assigner}`,
-      icon: '/icon-192.png',
-      badge: '/icon-192.png',
+      icon: '/img/icon-192.png',
+      badge: '/img/icon-192.png',
       vibrate: [200, 100, 200],
       data: { url: '/' }
     });
